47ffbe951026f5ecea877f6c158ddc6699f14599
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-11-23 10:25:05 +0100

    helloworld added

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+export default function HelloWorld() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <h2>At least I'm having fun 🎉</h2>
+            <p>
+                Edit <code>src/components/HelloWorld.jsx</code> and save to test
+                HMR
+            </p>
+        </>
+    );
+}
