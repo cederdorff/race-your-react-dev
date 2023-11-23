dfecd8091591a020b2f0291ae3d0a9a592e0a0bf
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-11-23 09:30:38 +0100

    install firebase and config

---
+++ src/config/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBtb9ldegWT4AIjY7jyO3tlDrJEQ7VWMJs",
+    authDomain: "race-your-react-dev.firebaseapp.com",
+    projectId: "race-your-react-dev",
+    storageBucket: "race-your-react-dev.appspot.com",
+    messagingSenderId: "1062364011620",
+    appId: "1:1062364011620:web:253762d13dadd131be5371"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
